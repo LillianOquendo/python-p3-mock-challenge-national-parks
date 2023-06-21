52210193e15a6b8bb2e8aeb7461b29e2c9860a9d
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-06-21 15:11:42 -0400

    Python Mock CC National Parks

--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -4,5 +4,42 @@ class Trip:
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
-        self.start_date = start_date
-        self.end_date = end_date
+        self.start_date = str(start_date)
+        self.end_date = str(end_date)
+        #Everybody knows where everybody has been
+        #vvv this appends this specific trip into visitor
+        self.visitor.trips(self)
+        #vvv this appends this national_park into visitor
+        self.visitor.national_parks(self.national_park)
+        #vvv this appends this specific trip into visitor
+        self.national_park.trips(self)
+        #vvv this appends this national_park into visitor
+        self.national_park.visitors(self.visitor)
+        
+        #trip is being called to do everything so we can consider it to be the single source of truth
+        #since its the SoT we need to make sure it inserts itself and visitor and national_park into
+        #the other classes. 
+
+    @property
+    def visitor (self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor (self, new_visitor):
+        from classes.visitor import Visitor
+        if new_visitor and isinstance(new_visitor, Visitor):
+            self._visitor = new_visitor
+        else:
+            raise Exception
+
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park (self, new_national_park):
+        from classes.national_park import NationalPark
+        if new_national_park and isinstance(new_national_park, NationalPark):
+            self._national_park = new_national_park
+        else:
+            raise Exception
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,11 +2,32 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self._trips = [] #remember to bring in _ if it's not being brought in as a constructor
+        self._national_parks = []
+
+    @property
+    def name(self):
+        return self._name  
+
+    @name.setter
+    def name (self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 15 and not hasattr(self,'name'):
+            #read me didnt say visitor names could not be changed. the error message did
+            self._name = new_name
+        else:
+            raise Exception
         
+
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
+        
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if new_national_park and isinstance(new_national_park, NationalPark) and new_national_park not in self._national_parks:
+            #to find the unique list you need to tell the system to check that the new park is not in the original park array
+            self._national_parks.append(new_national_park)
+        return self._national_parks
